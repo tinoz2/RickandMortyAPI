1fa8a27916a63c6e31b9272a38d0750dd885d990
Santino <mellamoalroberto@gmail.com>
2023-09-11 11:12:51 -0300

    Change1

---
+++ main.js
@@ -0,0 +1,36 @@
+function getCharacters(done) {
+    const results = fetch("https://rickandmortyapi.com/api/character")
+    
+        results
+        .then(response => response.json())
+        .then(data => {
+            done(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
+
+getCharacters(data => {
+    
+    data.results.forEach(personaje => {
+
+        const div = document.createElement("div");
+        div.classList.add("container-img");
+        div.innerHTML = `
+        <div class="container-img">
+            <img class="img" src="${personaje.image}" alt="">
+            <div class="container-stats">
+                <h2 class="h2">${personaje.name}</h2>
+                <div class="container-species">
+                    <small class="alive">${personaje.status} -</small>
+                    <small class="alive">Human</small>
+                </div>
+                <h3 class="place">Earth</h3>
+            </div>
+        </div>
+        `
+        const main = document.getElementById("main");
+        main.append(div);
+    })
+});
